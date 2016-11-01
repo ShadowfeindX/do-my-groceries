1b807d695a8d0e65eefb0e91cfa2369f564cbe63
ShadowfeindX <shadowfeind@programmer.net>
2016-11-01 14:12:04 -0400

    main updated

--- main.elm
+++ main.elm
@@ -3,4 +3,23 @@ module Main exposing (..)
 import Html
 
 main =
-  Html.div [][ Html.text "Hello from Elm!" ]
+  let
+    text = "Hello from Elm!"
+  in
+    Html.div [][ view model ]
+
+
+model =
+  [
+    ("Apples", 1.45),
+    ("Bananas", 1.22),
+    ("Oreos", 2.67),
+    ("Chicken Breast", 5.77)
+  ]
+
+view model =
+  let
+    display (item,price) =
+      Html.li [][ Html.text (item ++ " - $" ++ toString(price)) ]
+  in
+    Html.ul [] (List.map display model)
